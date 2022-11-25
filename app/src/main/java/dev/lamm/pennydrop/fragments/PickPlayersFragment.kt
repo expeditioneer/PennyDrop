115399709927f6e95aa39b786ea8190da2cb0948
Dennis Lamm <expeditioneer@gentoo.org>
2022-11-25 17:58:59 +0100

    added Bind ViewModel to Fragment PickPlayers

--- app/src/main/java/dev/lamm/pennydrop/fragments/PickPlayersFragment.kt
+++ app/src/main/java/dev/lamm/pennydrop/fragments/PickPlayersFragment.kt
@@ -5,16 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import dev.lamm.pennydrop.databinding.FragmentPickPlayersBinding
+import dev.lamm.pennydrop.viewmodels.PickPlayersViewModel
 
 
 class PickPlayersFragment : Fragment() {
+
+    private val pickPlayersViewModel by activityViewModels<PickPlayersViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentPickPlayersBinding.inflate(inflater, container, false)
+        val binding = FragmentPickPlayersBinding
+            .inflate(inflater, container, false)
+            .apply {
+                this.vm = pickPlayersViewModel
+            }
 
         return binding.root
     }
