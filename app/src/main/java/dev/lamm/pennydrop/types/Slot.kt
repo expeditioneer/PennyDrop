6c9f2a3b1b1a12151a0247bd715d00b69ae4dc38
Dennis Lamm <expeditioneer@gentoo.org>
2022-11-28 20:21:59 +0100

    Game bind ViewModel to Fragment

---
+++ app/src/main/java/dev/lamm/pennydrop/types/Slot.kt
@@ -0,0 +1,8 @@
+package dev.lamm.pennydrop.types
+
+data class Slot(
+    val number: Int,
+    val canBeFilled: Boolean = true,
+    var isFilled: Boolean = false,
+    var lastRolled: Boolean = false
+)
--- app/src/main/java/dev/lamm/pennydrop/viewmodels/GameViewModel.kt
+++ app/src/main/java/dev/lamm/pennydrop/viewmodels/GameViewModel.kt
@@ -1,12 +1,39 @@
 package dev.lamm.pennydrop.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dev.lamm.pennydrop.types.Player
+import dev.lamm.pennydrop.types.Slot
 
-class GameViewModel: ViewModel() {
+class GameViewModel : ViewModel() {
     private var players: List<Player> = emptyList()
+
+    val slots = MutableLiveData((1..6).map { slotNum ->
+        Slot(slotNum, slotNum != 6)
+    })
+
+    val currentPlayer = MutableLiveData<Player?>()
+
+    val canRoll = MutableLiveData(false)
+    val canPass = MutableLiveData(false)
+
+    val currentTurnText = MutableLiveData("")
+    val currentStandingText = MutableLiveData("")
 
     fun startGame(playersForNewGame: List<Player>) {
         this.players = playersForNewGame
+        this.currentPlayer.value =
+            this.players.firstOrNull().apply {
+                this?.isRolling = true
+            }
+        this.canRoll.value = true
+    }
+
+    fun roll() {
+        // TBD
+    }
+
+    fun pass() {
+        // TBD
     }
 }
