5af6b035b5d2ebf9703eabc20673e33801da502e
Dennis Lamm <expeditioneer@gentoo.org>
2022-11-29 07:11:09 +0100

    function 'startGame' added

--- app/src/main/java/dev/lamm/pennydrop/viewmodels/GameViewModel.kt
+++ app/src/main/java/dev/lamm/pennydrop/viewmodels/GameViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dev.lamm.pennydrop.types.Player
 import dev.lamm.pennydrop.types.Slot
+import dev.lamm.pennydrop.types.clear
 
 class GameViewModel : ViewModel() {
     private var players: List<Player> = emptyList()
@@ -20,13 +21,28 @@ class GameViewModel : ViewModel() {
     val currentTurnText = MutableLiveData("")
     val currentStandingText = MutableLiveData("")
 
+    private fun generateCurrentStandings(
+        players: List<Player>,
+        headerText: String = "Current Standings:"
+    ) = players.sortedBy { it.pennies }
+        .joinToString(separator = "\n", prefix = "$headerText\n") {
+            "\t${it.playerName} - ${it.pennies} pennies"
+        }
+
     fun startGame(playersForNewGame: List<Player>) {
         this.players = playersForNewGame
-        this.currentPlayer.value =
-            this.players.firstOrNull().apply {
-                this?.isRolling = true
-            }
-        this.canRoll.value = true
+        this.currentPlayer.value = this.players.firstOrNull().apply {
+            this?.isRolling = true
+        }
+
+        canRoll.value = true
+        canPass.value = false
+
+        slots.value?.clear()
+        slots.notifyChange()
+
+        currentTurnText.value = "The game has begun!\n"
+        currentStandingText.value = generateCurrentStandings(this.players)
     }
 
     fun roll() {
@@ -36,4 +52,8 @@ class GameViewModel : ViewModel() {
     fun pass() {
         // TBD
     }
+}
+
+private fun <T> MutableLiveData<List<T>>.notifyChange() {
+    this.value = this.value
 }
