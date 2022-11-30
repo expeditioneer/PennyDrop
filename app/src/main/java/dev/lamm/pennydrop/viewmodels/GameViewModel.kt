ba32661ae08a6b9c5cf5a61e94e65e003f5f25e9
Dennis Lamm <expeditioneer@gentoo.org>
2022-11-30 18:46:10 +0100

    GameViewModel added functionality

--- app/src/main/java/dev/lamm/pennydrop/viewmodels/GameViewModel.kt
+++ app/src/main/java/dev/lamm/pennydrop/viewmodels/GameViewModel.kt
@@ -2,11 +2,15 @@ package dev.lamm.pennydrop.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.lamm.pennydrop.game.GameHandler
+import dev.lamm.pennydrop.game.TurnEnd
+import dev.lamm.pennydrop.game.TurnResult
 import dev.lamm.pennydrop.types.Player
 import dev.lamm.pennydrop.types.Slot
 import dev.lamm.pennydrop.types.clear
 
 class GameViewModel : ViewModel() {
+    private var clearText = false
     private var players: List<Player> = emptyList()
 
     val slots = MutableLiveData((1..6).map { slotNum ->
@@ -29,6 +33,76 @@ class GameViewModel : ViewModel() {
             "\t${it.playerName} - ${it.pennies} pennies"
         }
 
+    private fun updateSlots(
+        result: TurnResult,
+        currentSlots: List<Slot>,
+        lastRoll: Int
+    ) {
+        if (result.clearSlots) {
+            currentSlots.clear()
+        }
+
+        currentSlots.firstOrNull { it.lastRolled }?.apply { lastRolled = false }
+
+        currentSlots.getOrNull(lastRoll - 1)?.also { slot ->
+            if (!result.clearSlots && slot.canBeFilled) slot.isFilled = true
+
+            slot.lastRolled = true
+        }
+
+        slots.notifyChange()
+    }
+
+    private fun generateTurnText(result: TurnResult): String {
+        if (clearText) currentTurnText.value = ""
+        clearText = result.turnEnd != null
+
+        val currenText = currentTurnText.value ?: ""
+        val currentPlayerName = result.currentPlayer?.playerName ?: "???"
+
+        return when {
+            result.isGameOver ->
+                """
+                    |Game over
+                    |$currentPlayerName is the winner!
+                    |
+                    |${generateCurrentStandings(this.players, "Final scores:\n")}
+                    }}
+                """.trimMargin()
+            result.turnEnd == TurnEnd.Bust -> "$currenText\n$currentPlayerName is busted."
+            result.turnEnd == TurnEnd.Pass -> "$currenText\n$currentPlayerName passes."
+            result.lastRoll != null -> "$currenText\n$currentPlayerName rolled a ${result.lastRoll}."
+            else -> ""
+        }
+    }
+
+    private fun updateFromGameHandler(result: TurnResult) {
+        if (result.currentPlayer != null) {
+            currentPlayer.value?.addPennies(result.coinChangeCount ?: 0)
+            currentPlayer.value = result.currentPlayer
+            this.players.forEach { player ->
+                player.isRolling = result.currentPlayer == player
+            }
+        }
+
+        if (result.lastRoll != null) {
+            slots.value?.let { currentSlots ->
+                updateSlots(result, currentSlots, result.lastRoll)
+            }
+        }
+
+        currentTurnText.value = generateTurnText(result)
+        currentStandingText.value = generateCurrentStandings(this.players)
+
+        canRoll.value = result.canRoll
+        canPass.value = result.canPass
+
+        if (!result.isGameOver && result.currentPlayer?.isHuman == false) {
+            canRoll.value = false
+            canPass.value = false
+        }
+    }
+
     fun startGame(playersForNewGame: List<Player>) {
         this.players = playersForNewGame
         this.currentPlayer.value = this.players.firstOrNull().apply {
@@ -46,11 +120,19 @@ class GameViewModel : ViewModel() {
     }
 
     fun roll() {
-        // TBD
+        slots.value?.let { currentSlots ->
+            val currentPlayer = players.firstOrNull { it.isRolling }
+            if (currentPlayer != null && canPass.value == true) {
+                updateFromGameHandler(GameHandler.roll(players, currentPlayer, currentSlots))
+            }
+        }
     }
 
     fun pass() {
-        // TBD
+        val currentPlayer = players.firstOrNull { it.isRolling }
+        if (currentPlayer != null && canPass.value == true) {
+            updateFromGameHandler(GameHandler.pass(players, currentPlayer))
+        }
     }
 }
 
